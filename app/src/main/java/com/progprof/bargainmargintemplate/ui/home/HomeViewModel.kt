d5d8fe5c8b51af2a7ce06cdf4d593e7ba8c52452
NitaiTran <nitai.tran996@myci.csuci.edu>
2025-10-08 14:35:22 -0700

    Added Budget bar and a few getter methods for the actual budget class (placeholder methods)

--- app/src/main/java/com/progprof/bargainmargintemplate/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/progprof/bargainmargintemplate/ui/home/HomeViewModel.kt
@@ -3,11 +3,14 @@ package com.progprof.bargainmargintemplate.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.progprof.bargainmargintemplate.myCurrentBudget
+import com.progprof.bargainmargintemplate.updateRemainingBudget
 
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+
+        value = "Budget Remaining: " + 5
     }
     val text: LiveData<String> = _text
 }
