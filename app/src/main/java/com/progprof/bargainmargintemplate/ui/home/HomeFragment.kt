ab9a0a29c74b21bc6a6a942531cd0d9a6c3efd30
NitaiTran <nitai.tran996@myci.csuci.edu>
2025-10-12 20:50:24 -0700

    Added basic progress bar functionality that changes based on the remaining budget of the user

--- app/src/main/java/com/progprof/bargainmargintemplate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/progprof/bargainmargintemplate/ui/home/HomeFragment.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.progprof.bargainmargintemplate.databinding.FragmentHomeBinding
 
+
+
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,6 +23,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
@@ -32,6 +34,18 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val progressBarview: ProgressBar = binding.progressBarHome
+        homeViewModel.progressValue.observe(viewLifecycleOwner)
+        {
+            progressBarview.progress = it
+        }
+
+        homeViewModel.maxValue.observe(viewLifecycleOwner)
+        {
+            progressBarview.max = it
+        }
+
         return root
     }
 
