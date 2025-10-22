deaac305b02529999c3afb89105cc38d0c85d763
JMoraHi <jmorahi09@gmail.com>
2025-10-21 21:39:08 -0700

    Added AnalyticsScreen.kt, CategoriesScreen.kt.kt, ExpenseTrackingScreen.kt, HomeScreen.kt
    Edited MainActivity.kt, ScreenController.kt, BudgetScreen.kt,
    BudgetViewModel.kt

---
+++ app/src/main/java/com/progprof/bargainmargintemplate/ui/ExpenseTrackingScreen.kt
@@ -0,0 +1,25 @@
+package com.progprof.bargainmargintemplate.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun ExpenseTrackingScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Expense Tracking Screen")
+        Button(onClick = { navController.popBackStack() }) {
+            Text(text = "Back to Home")
+        }
+    }
+}
