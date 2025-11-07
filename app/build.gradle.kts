9a90a564643eb678ef24080078a1909d8a5bdbd2
mig-v <vlincinerate@gmail.com>
2025-11-07 15:48:45 -0800

    Merge branch 'develop' of https://github.com/NitaiTran/BargainMargin into Sprint3Miguel
    # Conflicts:
    #	.idea/workspace.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,11 @@ android {
     buildFeatures {
         compose = true
     }
+
 }
 
 dependencies {
+    implementation("androidx.compose.material:material-icons-extended-android:1.6.8")
     implementation("androidx.navigation:navigation-compose:2.8.0-beta01") //added sprint 2, Jose
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -50,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,6 +61,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 
 
 
